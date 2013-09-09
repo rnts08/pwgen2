2e5737487124caecf9b601f0e053eb6a9ff2ce8b
Timh B <timh@qvzhosting.com>
2013-09-09 16:31:07 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 pwgen2
 ======
 
-pwgen replacement based on p.assword.me (random passwords with curse-words). Visit http://p.assword.me for the full site!
+Linux pwgen replacement based on p.assword.me (random passwords with curse-words). Visit http://p.assword.me for the full site, please don't spam or abuse.
+
+- Installation - 
+* git clone https://github.com/rnts08/pwgen2.git
+* cd pwgen2 
+* pip install -r requirements.txt
+
+Run.
---
+++ pwgen2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import requests
+import sys
+import re
+import argparse
+import columnize
+
+def assword(length=12):
+    try:
+        num = int(inp)
+        if num < 8 or num > 16:
+            return "Number must be between 8 and 16"
+        num = str(num)
+    except:
+        num = "16"
+    payload = {'n': num}
+    r = requests.post("http://p.assword.me/", data=payload)
+    assword = re.search('h2>(.*)<\/h2', r.text).group(1).replace('<span>','').replace('</span>','')
+    return assword
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='(P)assword generator based off http://p.assword.me.')
+
+    parser.add_argument('iterations', metavar='N', type=int, default=1, help='Number of passwords to generate', nargs='?')
+    parser.add_argument('-s', dest='length', type=int, default=12, help='Length of each password')
+
+    args = parser.parse_args()
+    asswords = []
+    for n in xrange(0, args.iterations):
+       asswords.append(assword(args.length)) 
+
+    print(columnize.columnize(asswords, displaywidth=80))
